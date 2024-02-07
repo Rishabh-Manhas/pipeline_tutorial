ede35b7cbe7c7b7280466a4b5e4660c51d13dad1
Rishabh-Manhas <rajputrishabh851@gmail.com>
2024-02-07 12:54:53 +0530

    Goal Defined

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('One Piece is Real!'));
+app.get('/', (req, res) => res.send('One Piece is Real! Orewa Naru Kaizoku Oni'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
